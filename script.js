b9628f5f6d0cae7cac07afd51a04e790678168a2
Yusuf <Yusuf@gmail.com>
2021-03-21 14:00:20 +0300

    js

---
+++ script.js
@@ -0,0 +1,10 @@
+// вывод текста с alert()
+alert("Hello Word!")
+// вывод текста в консоль
+console.log("sueta")
+// переменные с правильным имемнем 
+let $ibah
+let _hajah95
+// переменные с неправильным именем
+let 95mahajah
+let ibah!!
